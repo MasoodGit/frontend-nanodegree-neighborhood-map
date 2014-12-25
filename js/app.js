800f4447b19f847bc0101e530d700ffdefc3db34
Faiçal Tchirou <ftchirou@gmail.com>
2014-12-25 16:53:18 +0000

    truncate long category names.

--- js/app.js
+++ js/app.js
@@ -83,6 +83,10 @@ var NeighborhoodViewModel = function () {
                     var icon = category.icon;
                     return icon.prefix + '44' + icon.suffix;
                 });
+
+                category.displayName = ko.computed(function () {
+                    return category.pluralName.length > 22 ? category.pluralName.substring(0, 18) + ' ...' : category.pluralName;
+                });
             });
 
             self.categories(categories);
