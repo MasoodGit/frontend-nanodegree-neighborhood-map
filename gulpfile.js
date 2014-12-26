24d95c6ab4fb3fb74e5a5b57356cebbe8f25c980
Faiçal Tchirou <ftchirou@gmail.com>
2014-12-26 11:19:29 +0000

    tabs replaced with spaces.

--- gulpfile.js
+++ gulpfile.js
@@ -13,48 +13,48 @@ gulp.task('build', ['html', 'css', 'js', 'img', 'vendor']);
  * Minifies HTML files and moves them to the build directory.
  */
 gulp.task('html', function () {
-	gulp.src('index.html')
-		.pipe(htmlmin({
-			removeComments: true,
-			collapseWhitespace: true,
-			minifyJs: true,
-			minifyCSS: true
-		}))
-		.pipe(gulp.dest('build'));
+    gulp.src('index.html')
+        .pipe(htmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            minifyJs: true,
+            minifyCSS: true
+        }))
+        .pipe(gulp.dest('build'));
 });
 
 /**
  * Minifies CSS files and moves them to the build directory.
  */
 gulp.task('css', function () {
-	gulp.src('css/*.css')
-		.pipe(cssmin())
-		.pipe(gulp.dest('build/css'));
+    gulp.src('css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('build/css'));
 });
 
 /**
  * Minifies JS files and moves them to the build directory.
  */
 gulp.task('js', function () {
-	gulp.src('js/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('build/js'));
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
 });
 
 /**
  * Moves all the images to the build directory.
  */
 gulp.task('img', function () {
-	gulp.src('img/*')
-		.pipe(gulp.dest('build/img'));
+    gulp.src('img/*')
+        .pipe(gulp.dest('build/img'));
 });
 
 /**
  * Moves the libs into the build directory.
  */
 gulp.task('vendor', function () {
-	gulp.src('lib/**/*')
-		.pipe(gulp.dest('build/lib'));
+    gulp.src('lib/**/*')
+        .pipe(gulp.dest('build/lib'));
 });
 
 /**
@@ -63,9 +63,9 @@ gulp.task('vendor', function () {
  * we want to have modernizr minified.
  */
 gulp.task('min-modernizr', function () {
-	gulp.src('lib/modernizr/modernizr.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('lib/modernizr'));
+    gulp.src('lib/modernizr/modernizr.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('lib/modernizr'));
 });
 
 /**
@@ -73,11 +73,11 @@ gulp.task('min-modernizr', function () {
  * if we want optimized images in the production version of the app.
  */
 gulp.task('optim-img', function () {
-	gulp.src('img/*')
-		.pipe(imageop({
-			optimizationLevel: 5,
-			progressive: true,
-			interlaced: true
-		}))
-		.pipe(gulp.dest('img'));
+    gulp.src('img/*')
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('img'));
 });
